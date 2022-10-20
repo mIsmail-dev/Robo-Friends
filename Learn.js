754331a27bfcff1c7e714e5726a08cee0fb34e7c
mIsmail-dev <muhammad.ismail@ocloudsolutions.net>
2022-10-20 16:57:14 +0500

    React Basics 2.0

--- Learn.js
+++ Learn.js
@@ -23,3 +23,17 @@
 // In class components, when re-renders happen it will only run the render method in which we have return but in functional Components, In re-render all the function runs again. It's not only return ftn.
 
 // UseState returns two values [value, setValue], So Re-renders happens when the value changes, React is smart enough to know that when the value changes. It doesn't re-render when u call setValue ftn, it re-renders when value changes. If the value is same it is not going to re-render i.e if you have value "a" and then you change it back to "a". It will not re-render.
+
+
+// Dom & Virtual Dom
+// Basically, React under the hood makes the javascript tree copy of realDom called Virtual Dom. Because it doesn't want to make any changes directly to the real dom bcz its any expensive process.
+
+// So, when a changing happens or State changes, React takes the snapShot of the virtual Dom and makes a copy of Virtual DOM - Virtual Dom Copy.
+// After that react batches all the states call on the basis of it's speed. After batching, It apply all these changes in the Virtual Dom Copy in a Optimal way possible. After applying all those changes, it compare the virtual Dom Copy with the snapShot of Virtual Dom. After Comparing, it apply all those changes (which are present b/w snapshot & VirtualDom Copy) to the Real Dom. That's why react is most Optimal & has more speed.
+
+// REACT, REACT DOM, JSX
+// JSX - it is a syntax extension to JavaScript. Basically, it's a syntatic Sugar. JSX allows us to write html in the react.
+// REACT -  When we create a component in react using jsx, React under the hood is using Rect.createElement Syntax. When the engine compiles our code, it converts all our code to the Rect.createElement Syntax.
+// REACT DOM - ReactDom is where we render our app compoent code.
+
+// When we add or remove an element in React, it reflows the dom tree. But, if we just change the value of a node, then it will only update that node. React is that smart. We can check this behaviour by going to Rendering in developer tools and then clicking the paint flashing. :)
